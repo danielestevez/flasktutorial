eb5e5ed327789ca9af07c036c95562c96d55739d
danielestevez <daniel.estevez@abiquo.com>
2013-01-20 19:56:54 +0100

    addding Login modules imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flaskext.login import LoginManager
+from flaskext.openid import OpenID
+from config import basedir
 
 ''' Creates the application object instancing Flask and imports 'views' module '''
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.setup_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
-from app import views, models
