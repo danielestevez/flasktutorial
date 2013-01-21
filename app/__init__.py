aef9ef11d2b797f59f0dec1a6beca44fd4e904f3
danielestevez <daniel.estevez@abiquo.com>
2013-01-21 22:23:16 +0100

    Added basic logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flaskext.login import LoginManager
-from flaskext.openid import OpenID
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 from config import basedir
+import logging
+
+
 
 ''' Creates the application object instancing Flask and imports 'views' module '''
 app = Flask(__name__)
 app.config.from_object('config')
+
+file_handler = logging.FileHandler('app.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+
 db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.setup_app(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+app.logger.info('Flask Tutorial app initialized')
 
